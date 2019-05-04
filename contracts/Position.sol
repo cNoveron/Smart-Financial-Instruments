f2160a48e96f6ca030cd50fe068c3cd40f8c961f
carlosescom <carlos.escom.ipn@gmail.com>
2019-05-03 23:25:21 -0500

    [ADD] Position: Fields & logic

--- contracts/Position.sol
+++ contracts/Position.sol
@@ -3,5 +3,22 @@ pragma solidity >=0.4.21 <0.6.0;
 contract Position {
 
   mapping(address => uint) balance;
+  uint8 public positionType;
+  uint256 public paperProfit;
+  address trader;
+
+  constructor (uint8 _positionType) public {
+    require(
+      0 == _positionType || 1 == _positionType,
+      "Position constructor: PositionType must be either 0 (short) or 1 (long)"
+    );
+    positionType = _positionType;
+    trader = msg.sender;
+  }
+
+  function closePosition() public {
+    require(trader == msg.sender,"Position.closePosition: Only the trader who opened this position can close it");
+    // more logic...
+  }
 
 }
