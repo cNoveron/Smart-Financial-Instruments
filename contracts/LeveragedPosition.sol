8db32bddc7f5afcad93ea399d017eb12d389f35e
carlosescom <carlos.escom.ipn@gmail.com>
2019-05-05 20:59:53 -0500

    [UPDATE] LeveragedPosition < Position

--- contracts/LeveragedPosition.sol
+++ contracts/LeveragedPosition.sol
@@ -11,12 +11,12 @@ contract LeveragedPosition is Collateralized, Position {
   uint equity;
 
   constructor(
-    uint8 _positionType,
+    bool _positionType,
     address _collateralSymbol,
     uint _ratio, address _trader, uint256 _tradersFunds
   )
-  Position(_positionType)
   Collateralized(_collateralSymbol)
+  Position(_positionType)
   public payable {
     require(1 < _ratio, "Leveraged constructor: Ratio must be greater than 1");
     require(_trader != address(0), "Leveraged constructor: Address zero is universally invalid");
