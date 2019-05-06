291d173aec96355283a1fffdf46a49601efb60f6
carlosescom <carlos.escom.ipn@gmail.com>
2019-05-06 18:34:58 -0500

    [UPDATE] Positions: Security + logic

--- contracts/Positions.sol
+++ contracts/Positions.sol
@@ -1,8 +1,11 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import "./SafeMath.sol";
 import "./Ownable.sol";
 
 contract Positions is Ownable {
+
+  using SafeMath for uint256;
 
   uint256 openingCost;
   uint256 equity;
@@ -14,28 +17,46 @@ contract Positions is Ownable {
     string positionType;
     uint256 paperProfit;
     address borrower;
+    bool open;
   }
 
   constructor () internal {
+    openingCost = 0;
+    equity = 0;
+    listSize = 0;
   }
 
   function open(bool _isLong, address _borrower)
-  internal returns(uint256) {
+  internal onlyOwner() returns(uint256) {
     require(
       listSize < MAX_LIST_SIZE,
       "Positions.open: MAX_LIST_SIZE reached"
     );
-    return listSize = list.push(Position(_isLong ? "LONG" : "SHORT", 0, _borrower));
+    return _open(_isLong, _borrower);
   }
 
-  function settle(uint positionId) internal {
+  function _open(bool _isLong, address _borrower)
+  private returns(uint256) {
+    openingCost.add(msg.value);
+    return listSize = list.push(Position(_isLong ? "LONG" : "SHORT", 0, _borrower, true));
+  }
+
+  function settle(uint positionId)
+  internal {
+    require(
+      list[positionId].open,
+      "Position.settle: Only the borrower who opened this position can close it"
+    );
     require(
       msg.sender == list[positionId].borrower,
       "Position.settle: Only the borrower who opened this position can close it"
     );
-    _settle();
+    _settle(positionId);
   }
 
-  function _settle() private;
+  function _settle(uint positionId)
+  private {
+    list[positionId].open = false;
+  }
 
 }
